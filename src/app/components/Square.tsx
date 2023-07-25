4e360e0d0e45bcd488fb4137f477a21ba8e6d9d2
Felix Grant <fgrant505@outlook.com>
2023-07-25 17:04:47 +0100

    Find square colour based on position

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -2,17 +2,53 @@ import React from "react"
 
 interface Props {
     piece?: JSX.Element,
-    color: boolean
+    position: string
 }
+// Function to indicate the colour of the square given its position string. True
+// is returned for black/dark squares, and false is returned for white/light
+// squares
+function colourFromPosition(position: string): boolean {
+
+    const positionRegex = '/^[a-h][1-8]$/'
+    // Split position string into rank and file
+    if (position.length !== 2) {
+        throw "Position string must be of length 2"
+    } else if (!position.match(positionRegex)) {
+        throw "Position string must be a valid chess position"
+    }
+
+    // Get the value of the alphabetic file as a number, e.g. a is 1, b is 2, by
+    // getting its unicode value and subtracting 96, since the unicode value of
+    // a is 97
+    const file = position[0].charCodeAt(0) - 96
+    const rank = (position[1] as unknown) as number
+
+    // A position is white/light if the file is odd and the rank is even, or the
+    // file is even and the rank is odd
+    if (file % 2 !== 0) {
+        if (rank % 2 === 0) {
+            return false
+        } else {
+            return true
+        }
+    } else {
+        if (rank % 2 === 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
 export default function Square(props: Props): JSX.Element {
 
     const squareDimensions: number = 100;
-    let squareColor: string;
+    let squareColour: string;
 
-    if (props.color) {
-        squareColor = "SaddleBrown"
+    if (colourFromPosition(props.position)) {
+        squareColour = "SaddleBrown"
     } else {
-        squareColor = "BlanchedAlmond"
+        squareColour = "BlanchedAlmond"
     }
 
     const squareStyle = {
