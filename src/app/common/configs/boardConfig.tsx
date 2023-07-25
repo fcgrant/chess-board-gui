d9d1f9197ad846f30a4be3ff170c57a035450b8a
Felix Grant <fgrant505@outlook.com>
2023-07-25 18:04:05 +0100

    Render all squares with correct colours

--- src/app/common/configs/boardConfig.tsx
+++ src/app/common/configs/boardConfig.tsx
@@ -1,3 +1,3 @@
-import Piece from "@/app/components/Piece";
+import Piece from "../../components/Piece";
 
-const whiteKnight: JSX.Element = <Piece name="White Knight" imagePath="" value={3} />
+const whiteKnight: JSX.Element;
--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -1,33 +1,25 @@
 import React from "react";
 import Square from "./Square";
-import { List } from "postcss/lib/list";
 
 export default function Board(): JSX.Element {
 
-
     const boardDimension: number = 8;
-    let piecesOnBoard: Array<JSX.Element>;
-    let board: Array<JSX.Element>;
+    let position, file: string = "";
+    let rank: number;
+    let board: Array<JSX.Element> = [];
 
     for (let i = 0; i < boardDimension; i++) {
         for (let j = 0; j < boardDimension; j++) {
-
+            file = String.fromCharCode(97 + i)
+            rank = j + 1;
+            position = file + rank.toString();
+            board.push(<Square position={position} />)
         }
     }
+
     return (
-        <>
-            <Square
-                position="a1"
-            />
-            <Square
-                position="a2"
-            />
-            <Square
-                position="a3"
-            />
-            <Square
-                position="a4"
-            />
-        </>
+        <div>
+            {board}
+        </div>
     )
 }
